c0fafa6779fd7b83f5e7fc48ab284bb7b00b0045
sky <1714269831@qq.com>
2015-07-17 14:42:47 +0800

    <项目基础搭建>

---
+++ SkyFriend/src/com/example/skyfriend/util/Constants.java
@@ -0,0 +1,20 @@
+package com.example.skyfriend.util;
+
+public class Constants {
+	
+	public static final String DEFAULT_STRING_REQUEST_URL="www.baidu.com";
+//	public static final String DEFAULT_JSON_REQUEST_URL="http://image.baidu.com/channel/listjson?pn=0&rn=30&tag1=美女&tag2=全部&ie=utf8";
+	public static final String DEFAULT_JSON_REQUEST_URL="http://image.baidu.com/channel/listjson?pn=0&rn=30&tag1=%E7%BE%8E%E5%A5%B3&tag2=%E5%85%A8%E9%83%A8&ie=utf8";
+	public static final String DEFAULT_XML_REQUEST_URL="http://flash.weather.com.cn/wmaps/xml/china.xml";
+	public static final String DEFAULT_POST_REQUEST_URL="webservice.webxml.com.cn/WebServices/MobileCodeWS.asmx/getMobileCodeInfo";
+	
+	
+	private Constants() {
+	}
+
+	
+	public static class Extra {
+		public static final String FRAGMENT_INDEX = "com.grumoon.volleydemo.FRAGMENT_INDEX";
+	}
+
+}
