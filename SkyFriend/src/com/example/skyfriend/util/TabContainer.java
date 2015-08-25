81808ed2ff0894b143fb2c6f2b77c7e407fc95af
sky <1714269831@qq.com>
2015-08-25 17:31:24 +0800

    啊啊

--- SkyFriend/src/com/example/skyfriend/util/TabContainer.java
+++ SkyFriend/src/com/example/skyfriend/util/TabContainer.java
@@ -1,4 +1,5 @@
 package com.example.skyfriend.util;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
@@ -8,22 +9,31 @@ import android.widget.LinearLayout;
 /**
  * 自定义选项卡
  */
-public class TabContainer extends LinearLayout{
+public class TabContainer extends LinearLayout {
 
 	@SuppressLint("NewApi")
 	public TabContainer(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		// TODO Auto-generated constructor stub
+		init();
 	}
 
 	public TabContainer(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
+		init();
 	}
 
 	public TabContainer(Context context) {
 		super(context);
-		// TODO Auto-generated constructor stub
+		init();
 	}
 
+	private void init() {
+		setOrientation(HORIZONTAL);
+	}
+	
+	public TabContainer addItem(int iconResId,int iconResIdUnchecked,String text){
+		
+		
+		return this;
+	}
 }
