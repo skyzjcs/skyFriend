58d65c6fc99414abb4240fed8d34bfe9d18cd03e
sky <1714269831@qq.com>
2015-09-10 16:44:42 +0800

    哈哈哈

---
+++ SkyFriend/src/com/example/skyfriend/activity/ActivityMain.java
@@ -0,0 +1,81 @@
+package com.example.skyfriend.activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+
+import com.example.skyfriend.R;
+import com.example.skyfriend.ui.belle.BelleFragment;
+
+public class ActivityMain extends ActivityBase implements OnPageChangeListener{
+	private List<Fragment> mFragments=new ArrayList<Fragment>();
+	private ViewPager mViewPager;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		initFragments();
+	}
+
+	private void initFragments() {
+		mViewPager=(ViewPager) findViewById(R.id.viewPager);
+		mFragments.add(new BelleFragment());
+		mViewPager.setAdapter(new MPagerAdapter());
+		mViewPager.setCurrentItem(0,false);
+		mViewPager.setOnPageChangeListener(this);
+	}
+
+	/**
+	 * 自定义ViewPager适配器
+	 * @author caiweijian
+	 */
+	public class MPagerAdapter extends FragmentStatePagerAdapter {
+
+        public MPagerAdapter() {
+            super(getSupportFragmentManager());
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+    }
+
+	@Override
+	public void onPageScrollStateChanged(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onPageScrolled(int arg0, float arg1, int arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onPageSelected(int position) {
+		setFragmentUserVisibleHint(position);
+	}
+
+	private void setFragmentUserVisibleHint(int position) {
+		for(int i=0;i<mFragments.size();i++){
+			if(i==position){
+				mFragments.get(i).setUserVisibleHint(true);
+			}else{
+				mFragments.get(i).setUserVisibleHint(false);
+			}
+		}
+	}
+}
--- SkyFriend/src/com/example/skyfriend/activity/base/ActivityBase.java
+++
@@ -1,7 +0,0 @@
-package com.example.skyfriend.activity.base;
-import android.support.v4.app.FragmentActivity;
-
-
-public class ActivityBase extends FragmentActivity{
-
-}
